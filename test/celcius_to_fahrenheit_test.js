950b1a6cad91a4c51fde91c6a0572ec890d676eb
Kyle <k.yasumiishi@gmail.com>
2018-06-14 16:54:24 -0700

    Started celcius_to_fahrenheit_test.js

--- test/celcius_to_fahrenheit_test.js
+++ test/celcius_to_fahrenheit_test.js
@@ -1,10 +1,29 @@
 const expect = require('chai').expect;
 const celcius_to_fahrenheit = require('../algorithms/celcius_to_fahrenheit');
 
-console.log(celcius_to_fahrenheit.convertToF);
+const convertToF = celcius_to_fahrenheit.convertToF;
 
-describe('', function() {
-    it('', function() {
-        expect().to.equal();
+describe('convertToF', function() {
+    describe('return type', function() {
+        it('should return a number', function() {
+            expect(convertToF(0)).to.be.a('number');
+        });
+    });
+    describe('return value', function() {
+        it('should return a value of -22 when convertToF(-30)', function() {
+
+        });
+        it('should return a value of 14 when convertToF(-10)', function() {
+
+        });
+        it('should return a value of 32 when convertToF(0)', function() {
+
+        });
+        it('should return a value of 68 when convertToF(20)', function() {
+
+        });
+        it('should return a value of 86 when convertToF(30)', function() {
+
+        });
     });
 });
