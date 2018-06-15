448c439006ef729d1bf8c6917c3d1432dea36853
Kyle <k.yasumiishi@gmail.com>
2018-06-14 19:21:55 -0700

    Passed longest_word

--- algorithms/factorialize_number.js
+++ algorithms/factorialize_number.js
@@ -10,14 +10,3 @@ factorialize(5);
 
 
 module.exports = { factorialize: factorialize };
-
-
-// don't worry about negative numbers - only positive integers >= 0 will be provided
-
-// declare variable factorial
-
-// possible solution - create an array of range of integers from 1 to num, then multiply them together. 
-
-// is there a more elegant solution?
-// single for loop ( 1 - num ) num * i
-// iterate through positive integers in reverse direction starting from num, going down to 1, multiplying each on by variable.
---
+++ algorithms/longest_word.js
@@ -0,0 +1,27 @@
+function findLongestWordLength(str) {
+    let words = str.split(" ");
+    let longestWordLength = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longestWordLength) {
+            longestWordLength = words[i].length;
+        }
+    }
+    return longestWordLength;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+module.exports = { findLongestWordLength };    
+
+/* You can write concise object literal declaration using simple fields. 
+For example, instead of { findLongestWordLength : findLongestWordLength }, you can 
+export the function simply by writing findLongestWordLength once, like so: { findLongestWordLength }.
+*/
+
+// create array of words in string - remove spaces - use split with a space as separator
+
+// declare variable longestWord = 0;
+
+// iterate through array and update longestWord;
+
+// return longestWord;
