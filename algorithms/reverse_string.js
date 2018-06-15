ce716620e914f2a0c9be68fa34af8b5c27005b27
Kyle <k.yasumiishi@gmail.com>
2018-06-14 18:39:48 -0700

    Passed reverse_string

--- algorithms/reverse_string.js
+++ algorithms/reverse_string.js
@@ -1,7 +1,26 @@
 function reverseString(str) {
-    return str;
+    let arrayOfStrings = str.split("");
+    let reversed = "";
+    for (let i = arrayOfStrings.length - 1; i >= 0; i--) {
+        reversed += arrayOfStrings[i];
+    }
+    return reversed;
 }
 
 reverseString("hello");
 
 module.exports = { reverseString: reverseString };
+
+
+
+// Create an array where each character of str is an element - how to turn a string into an array
+
+// let arrayOfStrings = str.split("");
+
+// Declare a variable that's an empty string 
+
+
+
+// Iterate through array in reverse and concatenate element to variable
+
+// return variable
